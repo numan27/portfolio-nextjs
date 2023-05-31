5db1a373a2f0d781aad83f3173c33af242e7a5b2
numan27 <numankhalil27@gmail.com>
2023-05-31 14:51:05 +0500

    UI changes

--- pages/JSON/ServicesList.js
+++ pages/JSON/ServicesList.js
@@ -4,32 +4,32 @@ const ServicesList = [
     {
         img: IMAGES.SERVICE_ICON_1,
         title: "Custom Web Development",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus sed explicabo, blanditiis mollitia iste dicta labore excepturi reiciendis facilis eos, sint deleniti impedit temporibus necessitatibus repudiandae illo.",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus sed explicabo, blanditiis mollitia iste dicta labore excepturi reiciendis facilis eos.",
     },
     {
         img: IMAGES.SERVICE_ICON_2,
         title: "Responsive Design",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus sed explicabo, blanditiis mollitia iste dicta labore excepturi reiciendis facilis eos, sint deleniti impedit temporibus necessitatibus repudiandae illo.",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus sed explicabo, blanditiis mollitia iste dicta labore excepturi reiciendis facilis eos.",
     },
     {
         img: IMAGES.SERVICE_ICON_3,
         title: "HTML/React to Figma",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus sed explicabo, blanditiis mollitia iste dicta labore excepturi reiciendis facilis eos, sint deleniti impedit temporibus necessitatibus repudiandae illo.",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus sed explicabo, blanditiis mollitia iste dicta labore excepturi reiciendis facilis eos.",
     },
     {
         img: IMAGES.SERVICE_ICON_4,
         title: "Web Design & UI/UX",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus sed explicabo, blanditiis mollitia iste dicta labore excepturi reiciendis facilis eos, sint deleniti impedit temporibus necessitatibus repudiandae illo.",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus sed explicabo, blanditiis mollitia iste dicta labore excepturi reiciendis facilis eos.",
     },
     {
         img: IMAGES.SERVICE_ICON_5,
         title: "E-Commerce Development",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus sed explicabo, blanditiis mollitia iste dicta labore excepturi reiciendis facilis eos, sint deleniti impedit temporibus necessitatibus repudiandae illo.",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus sed explicabo, blanditiis mollitia iste dicta labore excepturi reiciendis facilis eos.",
     },
     {
         img: IMAGES.SERVICE_ICON_6,
         title: "Bootstrap & React Themes Integration",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus sed explicabo, blanditiis mollitia iste dicta labore excepturi reiciendis facilis eos, sint deleniti impedit temporibus necessitatibus repudiandae illo.",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus sed explicabo, blanditiis mollitia iste dicta labore excepturi reiciendis facilis eos.",
     },
 ]
 
